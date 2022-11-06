6fa3a440d6618e6bd48e0c8165de2c3fef9642a1
MishkaMO <mishkamo@yandex.ru>
2022-11-06 13:07:41 +0300

    задача 1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+  int forestReserve = 1321458524;
+  System.out.println("Значение переменной forestReserve " + "с типом int равно " + forestReserve);
+        byte apartmentsBuilding = 112;
+        System.out.println("Значение переменной apartmentsBuilding " + "с типом byte равно " + apartmentsBuilding);
+        short sunflowerSeeds = 31256;
+        System.out.println("Значение переменной sunflowerSeeds " + "с типом short равно " + sunflowerSeeds);
+        long programBytes = 9213985321458652845L;
+        System.out.println("Значение переменной programBytes " + "с типом long равно " + programBytes);
+        float backpackWeight = 45;
+        System.out.println("Значение переменной backpackWeight " + "с типом float равно " + backpackWeight);
+        double weightApples = 3.2;
+        System.out.println("Значение переменной weightApples " + "с типом double равно " + weightApples);
+    }
+}
